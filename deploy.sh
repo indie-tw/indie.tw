a7a55a445eaa897b108f4e796da32fa54f02f0ad
wancat <wancat@wancat.cc>
2023-01-31 17:13:38 +0800

    Add icon

--- deploy.sh
+++
@@ -1,8 +0,0 @@
-#/bin/sh
-SERVER=linchpins
-DEST=/var/www/indie.tw
-
-hugo --cleanDestinationDir &&
-rsync --exclude '.git' --delete \
---chmod=Du=rwx,Dgo=rx,Fu=rw,Fog=r \
--rtvzP ./public/. $SERVER:$DEST
